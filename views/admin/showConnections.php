46fd06d655e1b8fc29e58ae3d2a0d1e5ffc48cf5
Jonas Latza <jonaslatza@gmail.com>
2015-09-08 20:51:01 +0200

    Partial _partial

--- views/admin/showConnections.php
+++ views/admin/showConnections.php
@@ -13,17 +13,17 @@
   </tr>
 
   <tr>
-  <td>" . $client->getId() . "</td>\n" .
-  <td><img src=\"" . $ts3FameworkPath . "images/viewer/" . $client->getIcon() . ".png\" alt=\"" . $client->getIcon() . "\" title=\"" . $client->getIcon() . "\" /></td>\n".
-       "  <td><a href=\"?page=clientinfo&amp;server=" . $ts3_VirtualServer->getId() . "&amp;client=" . $client->getId() . "\">" . htmlspecialchars($client) . "</a></td>\n" .
-       "  <td>" . $client["client_unique_identifier"] . "</td>\n" .
-       "  <td>" . $client["client_platform"] . "</td>\n" .
-       "  <td>" . $client["client_version"] . "</td>\n" .
-       "  <td><a href=\"?page=channelinfo&amp;server=" . $ts3_VirtualServer->getId() . "&amp;channel=" . $client['cid'] . "\">" . $channelList[$client['cid']] . "</a></td>\n" .
-       "  <td>" . $client["connection_client_ip"] . "</td>\n" .
-   "  <td>" . $uptime_diff->format('%ad %H:%I:%S'). "</td>\n" .
-   "  <td>" . $idle_diff->format('%H:%I:%S'). "</td>\n" .
-       "</tr>\n";
+    <td>$client->getId()</td>
+    <td><img src=" . $ts3FameworkPath . "images/viewer/" . $client->getIcon() . ".png\" alt=\"" . $client->getIcon() . "\" title=\"" . $client->getIcon() . "\" /></td>
+    <td><a href=\"?page=clientinfo&amp;server=" . $ts3_VirtualServer->getId() . "&amp;client=" . $client->getId() . "\">" . htmlspecialchars($client) . "</a></td>
+    <td>" . $client["client_unique_identifier"] . "</td>
+    <td>" . $client["client_platform"] . "</td>
+    <td>" . $client["client_version"] . "</td>
+    <td><a href=\"?page=channelinfo&amp;server=" . $ts3_VirtualServer->getId() . "&amp;channel=" . $client['cid'] . "\">" . $channelList[$client['cid']] . "</a></td>\n" .
+    <td>" . $client["connection_client_ip"] . "</td>
+    <td>" . $uptime_diff->format('%ad %H:%I:%S'). "</td>
+    <td>" . $idle_diff->format('%H:%I:%S'). "</td>
+</tr>
 //print_r($clinfo['connection_connected_time']);
        foreach ($clinfo as $key => $value) {
            echo "<!-- " . $key . " = " . $value." -->\n";
